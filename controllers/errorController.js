60ea9d692ccff6c6dd05604ce2f12a0ba5f05a75
mosaab-rady <mosp.rady@gmail.com>
2021-05-04 23:30:41 +0200

    updated error controller

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -28,20 +28,22 @@ const sendError = (err, req, res) => {
   // A) API
   if (req.originalUrl.startsWith('/api')) {
     // A) Operational, trusted error: send message to client
-    return res.status(err.statusCode).json({
-      status: err.status,
-      message: err.message,
+    if (err.isOperational)
+      return res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message,
+      });
+
+    // B) Programming or other unknown error: don't leak error details
+    // 1) Log error
+    console.error('ERROR 💥', err);
+    // 2) Send generic message
+    return res.status(500).json({
+      status: 'error',
+      message: 'Something went wrong!',
+      err: err,
     });
   }
-
-  // B) Programming or other unknown error: don't leak error details
-  // 1) Log error
-  console.error('ERROR 💥', err);
-  // 2) Send generic message
-  return res.status(500).json({
-    status: 'error',
-    message: 'Something went wrong!',
-  });
 };
 
 module.exports = (err, req, res, next) => {
@@ -50,9 +52,10 @@ module.exports = (err, req, res, next) => {
 
   let error = { ...err };
   error.message = err.message;
-  if (error.name === 'CastError') error = handleCastErrorDB(error);
+  if (error.kind === 'ObjectId') error = handleCastErrorDB(error);
   if (error.code === 11000) error = handleDuplicateFieldsDB(error);
-  if (error.name === 'ValidationError') error = handleValidationErrorDB(error);
+  if (error.errors) error = handleValidationErrorDB(error);
+  // if (error.name === 'ValidationError') error = handleValidationErrorDB(error);
   if (error.name === 'JsonWebTokenError') error = handleJWTError();
   if (error.name === 'TokenExpiredError') error = handleJWTExpiredError();
 
