1bc9b50f473070d5cfa6393648c68d24d93a979d
mosaab-rady <mosp.rady@gmail.com>
2021-06-02 11:42:49 +0200

    mern app

--- client/src/component/Map.js
+++ client/src/component/Map.js
@@ -1,5 +1,5 @@
 /* eslint-disable */
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 
 import mapboxgl from 'mapbox-gl/dist/mapbox-gl-csp';
 
