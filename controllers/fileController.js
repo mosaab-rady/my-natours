c8f71623069b92c2c93cb23e1d0695e204091e2b
mosaab-rady <mosp.rady@gmail.com>
2021-06-09 16:24:43 +0200

    version 1.0.0

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -41,13 +41,15 @@ exports.getImage = (req, res, next) => {
   if (req.params.filename) {
     fileName = req.params.filename;
     const findImgs = async () => {
-      await new Promise((resolve) => {
+      await new Promise((resolve, reject) => {
         gfs.find({ filename: fileName }).toArray((err, files) => {
           if (!files[0] || files.length === 0) {
-            return res.status(200).json({
-              status: 'success',
-              message: 'no files available',
-            });
+            return reject(
+              res.status(200).json({
+                status: 'success',
+                message: 'no files available',
+              })
+            );
           }
           resolve(gfs.openDownloadStreamByName(fileName).pipe(res));
         });
